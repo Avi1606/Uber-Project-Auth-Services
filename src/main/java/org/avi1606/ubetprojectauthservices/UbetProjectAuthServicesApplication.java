fc61dd4a3300d1c003651f2043c1e3fbd7567159
Avikumar <aviiiprajapati16@gmail.com>
2026-02-11 10:41:16 +0530

    First committ

---
+++ src/main/java/org/avi1606/ubetprojectauthservices/UbetProjectAuthServicesApplication.java
@@ -0,0 +1,13 @@
+package org.avi1606.ubetprojectauthservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbetProjectAuthServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbetProjectAuthServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/avi1606/ubetprojectauthservices/UbetProjectAuthServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.avi1606.ubetprojectauthservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbetProjectAuthServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
